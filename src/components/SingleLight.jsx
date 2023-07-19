c2f246865b8f4afa490fa14c045e188ef1a30db9
Gritrax <139714043+GTytkowski@users.noreply.github.com>
2023-07-19 16:21:24 +0200

    feat: randomize light colors

--- src/components/SingleLight.jsx
+++ src/components/SingleLight.jsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
 import LightTurnedOff from "/images/light-turned-off.svg"
 import LightBlue from "/images/light-blue.svg"
+import LightGreen from "/images/light-green.svg"
+import LightOrange from "/images/light-orange.svg"
+import LightRed from "/images/light-red.svg"
+import LightPink from "/images/light-pink.svg"
+import LightYellow from "/images/light-yellow.svg"
+
+const lights = [LightBlue, LightGreen, LightOrange, LightRed, LightPink, LightYellow]
 
 export const SingleLight = () =>{
     const[isOn, setOn] = useState(true);
 
-    const imageSrc = isOn ? LightBlue : LightTurnedOff;
+    const getRandomIndex = () => {
+        return Math.floor(Math.random() * lights.length);
+    }
+
+    const imageSrc = isOn ? lights[getRandomIndex()] : LightTurnedOff;
 
     const handleToggle = () => {
         setOn(!isOn)
