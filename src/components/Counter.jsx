d3fe42885207c1476a1582bafa30a8d5badbc09e
Gritrax <139714043+GTytkowski@users.noreply.github.com>
2023-07-19 16:11:36 +0200

    feat: generate grid dynamically

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+
 import { Text } from "./Text"
 import { IconAdd } from "./icons/IconAdd"
 import { IconMinus } from "./icons/IconMinus"
@@ -7,23 +7,15 @@ import styles from "./Counter.module.css"
 
 export const Counter = (props) =>
 {
-const [value, setValue] = useState(0);
 
-const handleIncrement = () => {
-    setValue(value + 1);
-};
-const handlerDecrement = () => {
-    if(value <= 0) return;
-    setValue(value - 1);
-};
     return (
         <div className={styles.wrapper}>
             <Text>
-                {value} {props.text}
+                {props.value} {props.text}
             </Text>
             <nav>
-            <IconMinus onClick = {handlerDecrement} />
-            <IconAdd onClick = {handleIncrement} />
+            <IconMinus onClick = {props.onDecrement} />
+            <IconAdd onClick = {props.onIncrement} />
             </nav>
         </div>
     )
